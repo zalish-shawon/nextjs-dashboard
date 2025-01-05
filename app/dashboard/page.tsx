4682c6d682c58b105106d98149385cf6f32f6a47
zalishmahmud6 <zalishmahmud6@gmail.com>
2025-01-05 14:29:03 +0600

    upated

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashBoardPage = () => {
+    return (
+        <div>
+            <h1>Hello Dashboard</h1>
+        </div>
+    );
+};
+
+export default DashBoardPage;
