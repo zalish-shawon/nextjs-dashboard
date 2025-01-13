40bcfe3693be9f531abe36420c4bddafa9b21cec
zalishmahmud6 <zalishmahmud6@gmail.com>
2025-01-13 18:08:01 +0600

    optimized

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -22,11 +22,9 @@ export default async function CardWrapper() {
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
-  
+
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
