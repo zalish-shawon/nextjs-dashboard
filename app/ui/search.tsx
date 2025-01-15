53c3a6c348afd60e180eeba7055c64ed8bd00833
zalishmahmud6 <zalishmahmud6@gmail.com>
2025-01-15 12:07:33 +0600

    added pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,27 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { hadUnsupportedValue } from 'next/dist/build/analysis/get-page-static-info';
-import { URLSearchParams } from 'next/dist/compiled/@edge-runtime/primitives/url';
 import { usePathname, useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/router';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-  const {replace} = useRouter(); 
 
-  function handleSearch(term: string) {
-    // console.log(term);
-    const params = new URLSearchParams(searchParams);
+  const handleSearch = (term: string) => {
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('page', '1');
+
     if (term) {
-      params.set('query', term);
+      params.set('query', term); 
     } else {
-      params.delete('query');
+      params.delete('query'); 
     }
-    replace(`${pathname}?${params.toString()}`);
-  }
 
+    // Use the window object to navigate (client-side navigation)
+    window.history.replaceState(null, '', `${pathname}?${params.toString()}`);
+  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -35,4 +34,5 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
+}
 }
